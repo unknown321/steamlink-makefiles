b713cdfdc017fc30491ff10090062409eafe747c
unknown321 <ohboyahat@gmail.com>
2017-12-17 23:21:11 +0300

    somewhat working cups

--- cups/build_steamlink.sh
+++ cups/build_steamlink.sh
@@ -3,6 +3,7 @@
 STEAMLINK_IP="192.168.10.109"
 VERSION="2.2.6"
 PROGNAME="cups"
+TARGET_DIR="/home/steam/opt"
 DOWNLOAD_URL="https://github.com/apple/cups/releases/download/v2.2.6/${PROGNAME}-${VERSION}-source.tar.gz"
 
 TOP=$(cd `dirname "${BASH_SOURCE[0]}"` && pwd)
@@ -13,6 +14,7 @@ if [ "${MARVELL_ROOTFS}" = "" ]; then
 	source "${MARVELL_SDK_PATH}/setenv.sh" || exit 1
 fi
 BUILD="${TOP}/${PROGNAME}-build"
+mkdir ${BUILD}
 SRC="${TOP}/${PROGNAME}-${VERSION}"
 
 #
@@ -29,12 +31,21 @@ pushd "${SRC}"
 "${SRC}"/configure $STEAMLINK_CONFIGURE_OPTS ; pwd || exit 1
 make || exit 1
 "${CROSS}"strip -s ${PROGNAME}
+make BUILDROOT=${BUILD} install 
 popd
-
+tar -cf ${PROGNAME}-build.tar ${PROGNAME}-build  
 #
 # All done!
 #
 echo "Build complete!"
 echo
 echo "To copy ${PROGNAME} to the Steam Link, run:"
-echo "scp \"${BUILD}/${PROGNAME}\" root@${STEAMLINK_IP}:/home/steam/bin/"
+echo "scp \"${PROGNAME}-build.tar\" root@${STEAMLINK_IP}:/home/steam/opt/"
+echo "unpack archive:"
+echo "cd /home/steam/opt/ && tar -xf ${PROGNAME}-build.tar"
+echo "add paths to your .bashrc:"
+echo "PATH=$PATH:~/bin:/home/steam/opt/cups/usr/bin:/home/steam/opt/cups/usr/sbin"
+echo "export LD_LIBRARY_PATH=:/home/steam/libs:/home/steam/opt/cups/usr/lib"
+echo "run:"
+echo "cupsd -c /home/steam/opt/cups/etc/cups/cupsd.conf -s /home/steam/opt/cups/etc/cups/cups-files.conf -f"
+echo "cupsctl --remote-admin"
